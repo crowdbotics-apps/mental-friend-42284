e7f2f8d45f5639bfec8e62c447a1835d77b6ea1b
Crowdbotics <team@crowdbotics.com>
2023-07-19 17:52:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mental-friend-42284",
+  url: "https://mental-friend-42284.botics.co",
+  api: "https://mental-friend-42284.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
